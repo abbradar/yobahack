a4ca18777d868c198e75b0f8e0b70a41b24e35a2
Sergey Sofeychuk <lolwat97@gmail.com>
2012-08-04 01:18:11 +0400

    Wrote some code for testing, the code doesn't work.

--- tests/eventqueuetest.cc
+++ tests/eventqueuetest.cc
@@ -3,3 +3,11 @@
 TEST_F(EventQueueTest, wat) {
   EXPECT_EQ(1,1);
 }
+
+TEST_F(EventQueueTest, FirstTickTest) {
+  EXPECT_EQ(1,dummy1);
+}
+
+TEST_F(EventQueueTest, SecondTickTest) {
+  EXPECT_EQ(2,dummy2);
+}
--- tests/eventqueuetest.h
+++ tests/eventqueuetest.h
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <functional>
 #include <gtest/gtest.h>
+#include <boost/heap/priority_queue.hpp>
 #include "../server/eventqueue.h"
 
 class EventQueueTest : public ::testing::Test {
   protected:
+    EventQueue eq1;
+    EventQueue eq2;
+    EventQueue eq3;
+    int dummy1;
+    int dummy2;
+    int dummy3=0;
+    void foo1(){dummy1=1;}
+    void foo2(){dummy2=2;}
+    void foo3(){dummy3=3;}
+    std::function<void()> bar1;
+    std::function<void()> bar2;
+    bar1=&foo1;
+    bar2=&foo2;
     virtual void SetUp() {
-      //write something here
+      eq1.store_function(bar1,0);
+      eq1.store_function(bar2,1);
+      eq1.tick();
     }
-  EventQueue eq1;
-  EventQueue eq2;
-  EventQueue eq3;
 };
